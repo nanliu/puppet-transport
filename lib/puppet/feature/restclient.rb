586bac80980aec925024282cd1fb095346473f69
Nan Liu <nan.liu@gmail.com>
2014-03-10 09:37:30 -0500

    Remove non-transport functionality.

--- lib/puppet/feature/restclient.rb
+++
@@ -1,4 +0,0 @@
-# Copyright (C) 2013 VMware, Inc.
-require 'puppet/util/feature'
-
-Puppet.features.add(:restclient, :libs => %w{rest_client})
--- lib/puppet/feature/vshield.rb
+++
@@ -1,4 +0,0 @@
-# Copyright (C) 2013 VMware, Inc.
-require 'puppet/util/feature'
-
-Puppet.features.add(:vshield, :libs => %w{gyoku nori})
